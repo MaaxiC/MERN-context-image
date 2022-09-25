64f0ae8cd8f9a4396318422c281af9819471f185
Maximiliano Carnero <maaxi.c@gmail.com>
2022-09-25 18:40:25 -0300

    Base URL added

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
+const BASEURL = process.env.BASEURL || "http://localhost:4000";
+
 export const getPostsRequest = async () =>
-  await axios.get("http://localhost:4000/posts");
+  await axios.get(BASEURL + "/posts");
 
 export const createPostRequest = async (post) => {
   const form = new FormData();
   for (let key in post) {
     form.append(key, post[key]);
   }
-  return await axios.post("http://localhost:4000/posts", form, {
+  return await axios.post(BASEURL + "/posts", form, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -16,10 +18,10 @@ export const createPostRequest = async (post) => {
 };
 
 export const deletePostRequest = async (id) =>
-  await axios.delete(`http://localhost:4000/posts/${id}`);
+  await axios.delete(BASEURL + `/posts/${id}`);
 
 export const getPostRequest = async (id) =>
-  await axios.get(`http://localhost:4000/posts/${id}`);
+  await axios.get(BASEURL + `/posts/${id}`);
 
 export const updatePostRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:4000/posts/${id}`, newFields);
+  await axios.put(BASEURL + `/${id}`, newFields);
